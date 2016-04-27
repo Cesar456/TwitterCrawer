9dcb3e21efb5756950e779093c4373e8bb5bd7fd
unknown <>
2016-04-27 16:48:25 +0800

    add crawer by id

--- crawer/Crawer.py
+++ crawer/Crawer.py
@@ -12,18 +12,40 @@ api = twitter.Api(consumer_key='EKEMZjnkpUu7p8CbICyFKnUfD',
                   access_token_secret='D7kbKR9N1rHdYmtnUa6CdPs9qt1gNy8rEsdAIFBoC4Rhu')
 
 # 获取需要爬取的用户数据，元组或列表
-data = getData.get_data_from_xls("C:\Users\Cesar\Desktop\data.xls")
+data = getData.get_id_from_xls("C:\Users\Cesar\Desktop\data.xls")
 
 # 定义存储路径
-folder_path = "F:/twitter4/"
+folder_path = "F:/twitterData/"
 
 
 def main():
     for per in data:
-        get_statu(per)
+        get_status_by_id(per)
 
 
-def get_statu(screen_name):
+def get_status_by_id(user_id):
+    print str(user_id) + " start crawler"
+    statuses = api.GetUserTimeline(user_id=user_id, count='200')
+    saveData.sava_status_to_xml(statuses, str(folder_path + str(user_id) + "/"))
+    totle = len(statuses)
+    if totle == 0:
+        print str(user_id)+'无数据'
+        return
+    max_id = statuses[totle - 1].id
+    while not totle < 10:
+        time.sleep(2)
+        statuses = api.GetUserTimeline(user_id=user_id, count='200', max_id=max_id)
+        saveData.sava_status_to_xml(statuses, str(folder_path + str(user_id) + "/"))
+        totle = len(statuses)
+        if totle == 0:
+            print str(user_id)+'完毕'
+            return
+        max_id = statuses[totle - 1].id
+        print "..."
+    print str(user_id) + " complete"
+
+
+def get_status(screen_name):
     print screen_name + " start crawler"
     statuses = api.GetUserTimeline(screen_name=screen_name, count='200')
     saveData.sava_status_to_xml(statuses, str(folder_path + screen_name + "/"))
--- crawer/GetData.py
+++ crawer/GetData.py
@@ -1,20 +1,31 @@
 # encoding=utf-8
 
 import xlrd
+import types
 
 """该类主要用于原始任务数据的获取，
 方法的返回值必须是一个用户Screen Name的列表或元组
 """
 
 
-def get_data_from_xls(filepath="data.xls"):
-    data = xlrd.open_workbook(filepath)
+def get_data_from_xls(file_path="data.xls"):
+    data = xlrd.open_workbook(file_path)
     sheet = data.sheets()[0]
     re = []
     for s in sheet.col_values(1):
-        if s.startswith("@"):
-            s = s.replace("@", "")
-            re.append(s.strip())
+        if str(s).startswith("@"):
+            s = str(s).replace("@", "")
+            re.append(float(str(s).strip()))
+    return re
+
+
+def get_id_from_xls(file_path="data.xls"):
+    data = xlrd.open_workbook(file_path)
+    sheet = data.sheets()[0]
+    re = []
+    for s in sheet.col_values(1):
+        if type(s) is types.FloatType:
+            re.append(int(s))
     return re
 
 
