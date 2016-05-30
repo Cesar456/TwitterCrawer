af4bd4e64b37420ce1b5aef9f536974dd5149c46
unknown <>
2016-05-30 14:38:46 +0800

    change method

--- crawer/friendShipCrawer.py
+++ crawer/friendShipCrawer.py
@@ -29,11 +29,14 @@ def get_following():
 def get_following_id():
     id_ = 1
     for uid in data:
-        print str(id_) + str(uid)
+        print "序号：" + str(id_) + "  userID：" + str(uid)
         id_ += 1
         followers = api.GetFriendIDs(user_id=uid)
+        print "用户数据获取成功正在存储"
         SaveData.sava_user_id_to_txt(users=followers, folder_path=folder_path + str(uid) + "/")
-        time.sleep(60)
+        print "用户数据已存储"
+        time.sleep(100)
+
 
 if __name__ == '__main__':
     get_following_id()
